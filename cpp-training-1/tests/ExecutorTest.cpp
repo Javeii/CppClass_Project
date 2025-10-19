4ff9b123f5b475a5fa4f8a685690ff37d74942b2
javeii <2794223100@qq.com>
2025-10-19 17:34:32 +0800

    correct the test cases for moving command

--- cpp-training-1/tests/ExecutorTest.cpp
+++ cpp-training-1/tests/ExecutorTest.cpp
@@ -11,7 +11,7 @@ bool operator==(const Pose& lhs, const Pose& rhs)
 {  // 为Pose定义==运算符重载，使得在测试中比较Pose对象变得简单明了， std::tie提高代码可读性
     return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
 }
-    //测试套件       测试用例名，清晰地描述了预期行为和测试的条件，更容易理解测试在验证什么
+// 测试套件       测试用例名，清晰地描述了预期行为和测试的条件，更容易理解测试在验证什么
 TEST(ExecutorTest, should_return_init_pose_when_without_command)
 {
     // given
@@ -41,14 +41,32 @@ TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
 }
 TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
 {
-
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+    // when
+    executor->Execute("M");
+    // then
+    const Pose target({-1, 0, 'W'});
+    ASSERT_EQ(target, executor->Query());
 }
 TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
 {
-
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+    // when
+    executor->Execute("M");
+    // then
+    const Pose target({0, 1, 'N'});
+    ASSERT_EQ(target, executor->Query());
 }
 TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
 {
-
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+    // when
+    executor->Execute("M");
+    // then
+    const Pose target({0, -1, 'S'});
+    ASSERT_EQ(target, executor->Query());
 }
 }  // namespace adas
