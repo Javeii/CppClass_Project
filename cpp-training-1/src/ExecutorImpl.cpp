bc3f3940734cfb867a8f87526e1fd7ebafe0effe
javeii <2794223100@qq.com>
2025-10-19 17:40:37 +0800

    test: Pass move command test cases

--- cpp-training-1/src/ExecutorImpl.cpp
+++ cpp-training-1/src/ExecutorImpl.cpp
@@ -11,6 +11,27 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
+    for (char command : commands) {
+        if (command == 'M') {
+            switch (pose.heading) {
+            case 'N':
+                pose.y += 1;
+                break;
+            case 'S':
+                pose.y -= 1;
+                break;
+            case 'E':
+                pose.x += 1;
+                break;
+            case 'W':
+                pose.x -= 1;
+                break;
+            // 默认情况可以处理未知方向
+            default:
+                break;
+            }
+        }
+    }
 }
 Pose ExecutorImpl::Query() const noexcept
 {
