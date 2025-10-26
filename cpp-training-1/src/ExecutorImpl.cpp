67b6d8b89c1b71e74d73a6dfefceb89e634403e5
javeii <2794223100@qq.com>
2025-10-26 15:07:31 +0800

    some simple implementations of turn commands

--- cpp-training-1/src/ExecutorImpl.cpp
+++ cpp-training-1/src/ExecutorImpl.cpp
@@ -12,7 +12,7 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose)
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
     for (char command : commands) {
-        if (command == 'M') {
+        if (command == 'M') {  // 移动指令
             switch (pose.heading) {
             case 'N':
                 pose.y += 1;
@@ -27,6 +27,42 @@ void ExecutorImpl::Execute(const std::string& commands) noexcept
                 pose.x -= 1;
                 break;
             // 默认情况可以处理未知方向
+            default:
+                break;
+            }
+        }
+        if (command == 'L') {  // 左转指令
+            switch (pose.heading) {
+            case 'N':
+                pose.heading = 'W';
+                break;
+            case 'S':
+                pose.heading = 'E';
+                break;
+            case 'E':
+                pose.heading = 'N';
+                break;
+            case 'W':
+                pose.heading = 'S';
+                break;
+            default:
+                break;
+            }
+        }
+        if (command == 'R') {  // 右转指令
+            switch (pose.heading) {
+            case 'N':
+                pose.heading = 'E';
+                break;
+            case 'S':
+                pose.heading = 'W';
+                break;
+            case 'E':
+                pose.heading = 'S';
+                break;
+            case 'W':
+                pose.heading = 'N';
+                break;
             default:
                 break;
             }
