6f37e2077762fa172eb217c3cce8c005df3b35b8
javeii <2794223100@qq.com>
2025-12-16 05:03:50 +0800

    add test cases for reverse command

--- cpp-training-1/tests/ExecutorReverseTest.cpp
+++ cpp-training-1/tests/ExecutorReverseTest.cpp
@@ -17,5 +17,77 @@ TEST(ExecutorReverseTest, should_return_x_minus_1_given_status_is_back_command_i
     const Pose target{-1, 0, 'E'};
     ASSERT_EQ(target, executor->Query());
 }
+TEST(ExecutorReverseTest, should_return_S_given_status_is_back_command_is_L_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BL");
+
+    // then
+    const Pose target{0, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorReverseTest, should_return_N_given_status_is_back_command_is_R_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BR");
+
+    // then
+    const Pose target{0, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorReverseTest, should_return_x_minus_2_given_command_is_BFM_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BFM");
+
+    // then
+    const Pose target{-2, 0, 'E'};
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorReverseTest, should_return_S_and_x_minus_1_given_command_is_BFL_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BFL");
+
+    // then
+    const Pose target{-1, 0, 'S'};
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorReverseTest, should_return_N_and_x_minus_1_given_command_is_BFR_and_facing_is_E)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+
+    // when
+    executor->Execute("BFR");
+
+    // then
+    const Pose target{-1, 0, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorReverseTest, should_return_y_plus_1_given_command_is_BBM_and_facing_is_N)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+
+    // when
+    executor->Execute("BBM");
+
+    // then
+    const Pose target{0, 1, 'N'};
+    ASSERT_EQ(target, executor->Query());
+}
 
 }  // namespace adas
